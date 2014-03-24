b32a52285b73f2c71ad3350f07e01b2b49d3c80b
OpenShift guest <iterasprint1@gmail.com>
2014-03-24 02:43:30 -0400

    hello world service

--- python/urls.py
+++ python/urls.py
@@ -4,10 +4,10 @@ from django.conf.urls import patterns, include, url
 # from django.contrib import admin
 # admin.autodiscover()
 
+import views
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'python.views.home', name='home'),
-    # url(r'^python/', include('python.foo.urls')),
+    url(r'^$', views.index, name='index')
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
