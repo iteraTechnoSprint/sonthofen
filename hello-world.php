63b4f37869df41fc03dd586ae7516884c5d583ff
OpenShift guest <iterasprint1@gmail.com>
2014-03-28 06:44:16 -0400

    Added file get contents

--- hello-world.php
+++ hello-world.php
@@ -1,4 +1,10 @@
 <?php
 header('Content-Type: text/plain');
-echo htmlspecialchars($_GET["x"]) . ',' . htmlspecialchars($_GET["y"]);
+
+
+$output = array (   "x" => htmlspecialchars($_GET["x"]),
+                    "y" => htmlspecialchars($_GET["y"]),
+                    "iteratec" => htmlspecialchars(file_get_contents("http://www.iteratec.de")));
+
+echo json_encode ($output);
 ?>
