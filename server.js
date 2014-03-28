50e69cc51d5477c38555bc1a1ac33c493ed5b73a
OpenShift guest <iterasprint1@gmail.com>
2014-03-28 06:45:25 -0400

    Added support for external data lookup

--- server.js
+++ server.js
@@ -6,14 +6,40 @@ var http = require('http');
 var path = require('path');
 var url = require("url");
 
-var server = http.createServer(function(request,response){  
-  var parsedUrl = url.parse(request.url, true); // true to get query as object
-  var queryAsObject = parsedUrl.query;
-  console.log(JSON.stringify(queryAsObject));
-  response.end(JSON.stringify(queryAsObject));
+var server = http.createServer(function(request,response) {  
+    var parsedUrl = url.parse(request.url, true); // true to get query as object
+    var queryAsObject = parsedUrl.query;
+    console.log(JSON.stringify(queryAsObject));
+
+   
+
+    getExternalRessource(function(resp) {
+        response.write(JSON.stringify(queryAsObject));
+        response.end(resp);
+    });
+
+  
 });
+
+
+var getExternalRessource = function(callback) {
+    var options = {
+      host: 'www.iteratec.de',
+      port: 80,
+      path: '/index.html'
+    };
+    
+    http.get(options, function(resp){
+        resp.on('data', callback);
+    }).on("error", function(e){
+        console.log("Got error: " + e.message);
+    });
+}
+
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
   console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
+
+
